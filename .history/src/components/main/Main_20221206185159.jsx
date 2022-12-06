ccbb99ba126e76ae3de5071ac2ec34b6603d8887
VladBog <you@example.com>
2022-12-06 18:56:18 +0200

    third commit

---
+++ .history/src/components/main/Main_20221206185159.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../header/Header';
+import './main.css';
+import bigFade6 from '../../assets/images/bigfade6.png';
+
+const Main = ({ activeBurger, setActiveBurger }) => {
+  return (
+    <div className="main" id="main">
+      {/* <Header activeBurger={activeBurger} setActiveBurger={setActiveBurger} />
+      <h2>FROM DUSK TO DAWN</h2>
+      <div>
+        <button className="main-button">Mint</button>
+        <button className="main-button">Connect Wallet</button>
+      </div> */}
+      {/* <img className="bigFade6" src={bigFade6} alt="bigFade6" /> */}
+    </div>
+  );
+};
+
+export default Main;
